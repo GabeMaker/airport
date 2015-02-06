6e8f3e4608a05686a0f443fe5a615a3bd7c6b3cf
gabe <GabeMaker@users.noreply.github.com>
2015-02-06 18:18:55 +0000

    refactor tests

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,19 +2,15 @@ require 'plane'
 
 describe Plane do
 
+  let(:plane) {Plane.new}
+
   it 'should not be flying when created' do
-    # create a plane
-    # check if it's flying
-    expect(Plane.new).not_to be_flying
+    expect(plane.flying).to eq false
   end
 
   it 'should be able to take off' do
-    # plane is not flying
-    # plane takes off
-    # plane is flying
-    plane = Plane.new
     plane.take_off
-    expect(plane).to be_flying 
+    expect(plane.flying).to eq true
   end
 
 end
